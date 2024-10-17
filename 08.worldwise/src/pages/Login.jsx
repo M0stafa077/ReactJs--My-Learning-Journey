7ac723cfa37088546f19eabf3eec1137ef5c1b16
Mostafa Asaad <ma4190243@gmail.com>
2024-10-17 22:49:04 +0300

    Added user authentication and app authorization-fake authgit add src/pages/AppLayout.jsx src/components/Spinner.jsx src/pages/Login.jsx src/contexts/authenticationContext.jsx

--- 08.worldwise/src/pages/Login.jsx
+++ 08.worldwise/src/pages/Login.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import Button from "../components/Button";
+import { useAuth } from "../contexts/authenticationContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
     // PRE-FILL FOR DEV PURPOSES
     const [email, setEmail] = useState("jack@example.com");
     const [password, setPassword] = useState("qwerty");
+    const { handleLogin, isAuthenticated } = useAuth();
+    const navigate = useNavigate();
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        handleLogin(email, password);
+    }
+    useEffect(() => {
+        if (isAuthenticated) navigate("/app");
+    }, [isAuthenticated]);
     return (
         <main className={styles.login}>
             <PageNav />
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <div className={styles.row}>
                     <label htmlFor="email">Email address</label>
                     <input
@@ -30,8 +42,8 @@ export default function Login() {
                     />
                 </div>
 
-                <div>
-                    <button>Login</button>
+                <div className={styles.loginBtn}>
+                    <Button type="primary">Login</Button>
                 </div>
             </form>
         </main>
